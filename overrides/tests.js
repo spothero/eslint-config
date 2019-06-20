47ba37132f8e9ef21a6021f6a462ecf3a2ef9e5d
Boiar Qin <boiar@spothero.com>
2019-06-20 17:43:56 -0500

    style: Reorder rules

--- overrides/tests.js
+++ overrides/tests.js
@@ -3,12 +3,12 @@ module.exports = {
         {
             files: ['**/*.spec.js', '**/*.test.js'],
             rules: {
+                'camelcase': 0,
                 'max-nested-callbacks': 0,
                 'no-unused-expressions': 0,
                 'no-console': 0,
                 'no-undefined': 0,
                 'react/jsx-handler-names': 0,
-                'camelcase': 0,
             }
         }
     ]
