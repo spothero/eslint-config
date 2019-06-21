c7d89aae0188c2b40fdd7df786a36376797f6611
boiarqin <boiar@spothero.com>
2019-06-21 09:57:38 -0500

    fix: Override rules that don't apply to test files

---
+++ overrides/tests.js
@@ -0,0 +1,15 @@
+module.exports = {
+    overrides: [
+        {
+            files: ['**/*.spec.js', '**/*.test.js'],
+            rules: {
+                'camelcase': 0,
+                'max-nested-callbacks': 0,
+                'no-unused-expressions': 0,
+                'no-console': 0,
+                'no-undefined': 0,
+                'react/jsx-handler-names': 0,
+            }
+        }
+    ]
+};
