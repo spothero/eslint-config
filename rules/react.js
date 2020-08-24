0fbebfd622564b2487cddfe1778f7ecdbca13e74
Matt <mprzybylski@gmail.com>
2020-08-24 10:46:14 -0700

    fix: Add displayName to react-sort-comp

--- rules/react.js
+++ rules/react.js
@@ -81,6 +81,7 @@ module.exports = {
                 ],
                 groups: {
                     lifecycle: [
+                        'displayName',
                         'propTypes',
                         'contextTypes',
                         'childContextTypes',
